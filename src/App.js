d201b473620532948e1bbc2afb01c924aac206b5
lexville <alekikangethe@gmail.com>
2019-01-08 14:47:30 +0300

    Add the movie component

--- src/App.js
+++ src/App.js
@@ -1,12 +1,38 @@
 import React, { Component } from "react";
+import logo from "./logo.svg";
 import "./App.css";
+import Movie from "./Movie";
+
+const movies = [
+  {
+    id: 1,
+    title: "Star Wars"
+  },
+  {
+    id: 2,
+    title: "Charmed"
+  },
+  {
+    id: 3,
+    title: "Vice"
+  }
+];
 
 class App extends Component {
   render() {
+    const renderMovies = movies.map(movie => (
+      <Movie key={movie.id} movie={movie} />
+    ));
     return (
-      <div className="App">
-        <h1>Lumdb</h1>
-      </div>
+      <React.Fragment>
+        <header className="App-header text-center">
+          <img src={logo} className="App-logo" alt="logo" />
+        </header>
+        <div className="container">
+          <h1>Movies</h1>
+          {renderMovies}
+        </div>
+      </React.Fragment>
     );
   }
 }
